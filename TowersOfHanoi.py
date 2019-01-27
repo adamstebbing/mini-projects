89b57fbb2634290cead7169a304ef834b17a005f
adamstebbing <AdamStebbing@gmail.com>
2019-01-27 01:28:31 -0600

    Added ThreadShed, a string manipulation project, and TowersOfHanoi, a stack manipulation project.

---
+++ TowersOfHanoi.py
@@ -0,0 +1,70 @@
+# Basic Towers of Hanoi Game Implementation
+# by Adam Stebbing on 1/23/2019
+# Based around CodeAcademy program which aims to test Student knowledge of Stacks
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+# Create the Stacks
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+
+# Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+while num_disks < 3:
+  num_disks = int(input("\nEnter a number greater than or equal to 3\n"))
+for x in range(num_disks,0,-1):
+  print(x)
+  left_stack.push(x)
+optimal_moves =2**num_disks-1
+print("\nThe fastest you can solve this game is in {} moves.".format(optimal_moves))
+
+# Get User Input
+def print_stacks():
+  print("\n\n\n...Current Stacks...")
+  for x in stacks:
+    x.print_items()
+
+def get_input():
+  choices = ['L', 'M', 'R']
+  while True:
+    for i in range(len(stacks)):
+      name = stacks[i].get_name()
+      letter = choices[i]
+      print("Enter {} for {}".format(letter, name))
+      
+    user_input = input("Input: ")
+    if user_input.upper() in choices:
+      for i in range(len(stacks)):
+        if user_input.upper() == choices[i]:
+          return stacks[i]
+    else:
+      print("\nInvalid Choice, Try Again.")
+  
+        
+# Play the Game
+num_user_moves = 0
+while stacks[2].get_size() != num_disks:
+  print_stacks()
+  while True:
+    print("\nWhich stack do you want to move from?\n")
+    from_stack = get_input()
+    print("\nWhich stack do you want to move to?\n")
+    to_stack = get_input()
+    if from_stack.is_empty() or from_stack == to_stack:
+      print("\nInvalid Move. Try Again")
+    elif to_stack.is_empty() or from_stack.peek() < to_stack.peek():
+      disk = from_stack.pop()
+      to_stack.push(disk)
+      num_user_moves += 1
+      break
+    else:
+      print("\nInvalid Move. Try Again")
+print_stacks()
+print("\nYou completed the game in {} moves, and the optimal number of moves is {}".format(num_user_moves, optimal_moves))
+  
